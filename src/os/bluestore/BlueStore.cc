decdf09b7e1214bddb0b7d7a4f39de81444f1b6c
J. Eric Ivancich <ivancich@redhat.com>
2018-07-19 16:28:04 -0400

    MAYBE: Turn off throttling at BlueStore level.

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -9040,7 +9040,9 @@ void BlueStore::_kv_sync_thread()
       // iteration there will already be ops awake.  otherwise, we
       // end up going to sleep, and then wake up when the very first
       // transaction is ready for commit.
+#if 0
       throttle_bytes.put(costs);
+#endif
 
       PExtentVector bluefs_gift_extents;
       if (bluefs &&
@@ -9369,7 +9371,9 @@ void BlueStore::_deferred_aio_finish(OpSequencer *osr)
       costs += txc->cost;
     }
     osr->qcond.notify_all();
+#if 0
     throttle_deferred_bytes.put(costs);
+#endif
     std::lock_guard<std::mutex> l(kv_lock);
     deferred_done_queue.emplace_back(b);
   }
@@ -9485,9 +9489,12 @@ int BlueStore::queue_transactions(
     handle->suspend_tp_timeout();
 
   auto tstart = mono_clock::now();
+#if 0
   throttle_bytes.get(txc->cost);
+#endif
   if (txc->deferred_txn) {
     // ensure we do not block here because of deferred writes
+#if 0
     if (!throttle_deferred_bytes.get_or_fail(txc->cost)) {
       dout(10) << __func__ << " failed get throttle_deferred_bytes, aggressive"
 	       << dendl;
@@ -9500,7 +9507,8 @@ int BlueStore::queue_transactions(
       }
       throttle_deferred_bytes.get(txc->cost);
       --deferred_aggressive;
-   }
+    }
+#endif
   }
   auto tend = mono_clock::now();
 
