81daf9bffccc1f7037f8e5b555021eb87794547f
yzhan298 <yzhan298@ucsc.edu>
2019-10-15 01:23:29 -0500

    modify the dev scripts

--- dev-scripts/verify_op_queue_size/run.sh
+++ dev-scripts/verify_op_queue_size/run.sh
@@ -4,4 +4,4 @@ sudo bin/ceph osd pool delete mybench mybench --yes-i-really-really-mean-it
 sudo ../src/stop.sh
 sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -b -d -n -x -l --without-dashboard
 sudo bin/ceph osd pool create mybench 150 150
-./sampling_perf_state.sh 16
+./sampling_perf_state.sh 128
--- dev-scripts/verify_op_queue_size/sampling_perf_state.sh
+++ dev-scripts/verify_op_queue_size/sampling_perf_state.sh
@@ -4,18 +4,20 @@
 bs=4096 #4k: 4096 #128k: 131072 #4m: 4194304
 os=4096 #4194304  #4096
 qdepth=$1
-time=10 #5mins=300
+time=300 #5mins=300
 parallel=1
 sampling_time=2 # second(s)
 
-run_name=t_test_${qdepth}
+run_name=test_${qdepth}
 osd_count=1
-shard_count=2
+shard_count=5
 temp=/tmp/load-ceph.$$
 
 CURRENTDATE=`date +"%Y-%m-%d %T"`
 #DATA_OUT_FILE="res_${qdepth}_${time}.csv"
-DATA_OUT_FILE="result.csv"
+DATA_OUT_FILE="result_${CURRENTDATE}.csv"
+printf "%s\n" ${CURRENTDATE} |  paste -sd ',' > ${DATA_OUT_FILE}
+printf '%s\n' "bs" "runtime" "concurrency" "throughput" "latency" |  paste -sd ',' >> ${DATA_OUT_FILE} 
 
 sudo bin/ceph daemon osd.0 perf reset osd >/dev/null 2>/dev/null
 
@@ -27,10 +29,12 @@ do_dump() {
     	sudo bin/ceph daemon osd.${o} dump_op_pq_state 2>/dev/null | tee $dump_opq | jq 'map(.op_queue_size)' >$temp
 	for s in $(seq 0 $(expr $shard_count - 1)) ; do
 		op_queue_size=$(jq ".[${s}]" $temp)
-		echo "${count}.${o}.${s} : ${op_queue_size}"
+		echo "count-${count}.osd-${o}.shard-${s} : ${op_queue_size}"
 	done
         dump_state="dump.state.${count}"
 	sudo bin/ceph daemon osd.0 perf dump 2>/dev/null | tee $dump_state
+
+	printf '%s\n' $bs $time $qdepth | paste -sd ',' >> ${DATA_OUT_FILE}
 done
 }
 
@@ -48,7 +52,7 @@ sleep 5
 starttime=`date +%s.%N`
 
 samples=$(expr $time/2 | bc -l)
-time_dump $samples $sampling_time &
+time_dump $samples $sampling_time > dump &
 
 for p in $(seq $parallel) ; do
     sudo bin/rados bench -p mybench -b ${bs} -o ${os} -t ${qdepth} --run-name=${run_name}_${p} ${time} write --run-name ${run_name}-${p} --no-cleanup > rados-bench-${qdepth}-${p} &
