07bc15ed851a44c03300b24ff137b833a48dab5a
yzhan298 <yzhan298@ubuntu.yzhan298-qv58943.declstore-pg0.wisc.cloudlab.us>
2019-10-28 02:01:18 -0500

    update scripts and results for verifying kv_queue batch size vs bluestore latency

--- dev-scripts/verify_kv_queue_size/scripts/loop_run.sh
+++ dev-scripts/verify_kv_queue_size/scripts/loop_run.sh
@@ -4,8 +4,8 @@
 #for qd in 1 4 8 16 32 48 64 80 96 112 128 144 160 256; do
 #for qd in 1 4 ;do
 for bt in "rados" "rbd" "fio"; do
-    #for qd in 1 4 8 16 32 48 64 80 96 112 128; do
-    for qd in 1 4 ;do
+    for qd in 1 4 8 16 32 48 64 80 96 112 128; do
+    #for qd in 1 4 ;do
 	./run.sh $qd $bt
     done
     python plot_single_dump.py
@@ -13,4 +13,6 @@ for bt in "rados" "rbd" "fio"; do
     dirname=${bt}_plot_qd${qdepth}_$(date +%F)
     mkdir -p data/${dirname} # create data if not created
     mv dump* data/${dirname} 
+    mv result-single-dump.csv data/${dirname}
+
 done
