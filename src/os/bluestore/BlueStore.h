5fb0de746be44fbecae66a91c51b33ea70a48059
yzhan298 <yzhan298@c220g2-010603.wisc.cloudlab.us>
2020-03-27 16:13:16 -0500

    add some comments in the IO path of deferred writes

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -1557,10 +1557,12 @@ public:
     utime_t time_finished; // time when finished
     utime_t time_aio_submit; // time when aio is submitted
     utime_t time_aio_done; // time when aio is done(calling state machine again)
+    utime_t time_deferred_io_submit; // starting time for deferred_writes 
     utime_t time_kvq_in; // time when txc is pushed into kv_queue
     utime_t time_kvq_out; // time when txc is swapped out from kv_queue
     utime_t time_measure_end; // end time for codel measurement
     utime_t aio_latency; // store the aio latency 
+    utime_t aio_dio_latency; // store both aio and dio latencies
     //std::chrono::time_point<mono_clock> time_kvq_in1;
     //std::chrono::time_point<mono_clock> time_kvq_out1; 
     //std::chrono::time_point<mono_clock> time_commit_done1;
