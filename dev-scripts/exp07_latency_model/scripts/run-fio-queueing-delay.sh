a78985add974709758ec3d12b241b8be24472dda
yzhan298 <yzhan298@ucsc.edu>
2020-04-10 19:21:33 -0500

    update the run-fio-queueing-delay.sh

--- dev-scripts/exp07_latency_model/scripts/run-fio-queueing-delay.sh
+++ dev-scripts/exp07_latency_model/scripts/run-fio-queueing-delay.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#sudo ../src/stop.sh
-#sudo bin/init-ceph stop
-#sleep 5
 
 # run rbd bench and collect result
-bs="4096"   #"131072"  # block size or object size (Bytes)
+bs="4096"   #"131072"  # block size 
 rw="randwrite"  # io type
 fioruntime=10  # seconds
 iototal="400m" # total bytes of io
@@ -18,11 +15,8 @@ pool="mybench"
 
 single_dump() {
     qdepth=$1
-    for o in $(seq 0 $(expr $osd_count - 1)) ; do
-        dump_state="dump-state-${qdepth}"
-        sudo bin/ceph daemon osd.0 perf dump 2>/dev/null > $dump_state
-	#avg_throughput_bench=$(grep "Bandwidth (MB/sec)" dump-rados-bench-${qdepth}) #| awk '{print $3}')
-	#bw=$(grep BW dump-fio-bench-${qdepth} |  awk '{print $3}') # | grep -o '[0-9]*')
+    dump_state="dump-state-${qdepth}"
+    sudo bin/ceph daemon osd.0 perf dump 2>/dev/null > $dump_state
 	clt_bw=$(echo "$(jq '.jobs[0].write.bw' dump-fio-bench-${qdepth}.json) / 1024" | bc -l) #client throughput MB/s
 	clt_lat=$(echo "$(jq '.jobs[0].write.lat_ns.mean' dump-fio-bench-${qdepth}.json) / 1000000000" | bc -l) # client latency seconds
 	osd_op_in_osd_lat=$(jq ".osd.osd_op_in_osd_lat.avgtime" $dump_state)
@@ -36,21 +30,21 @@ single_dump() {
 	bluestore_deferred_service_lat=$(jq ".bluestore.bluestore_dio_lat.avgtime" $dump_state)
     
     printf '%s\n' $bs $fioruntime $qd $clt_bw $clt_lat $osd_op_in_osd_lat $osd_op_queueing_time $bluestore_simple_writes_lat $bluestore_deferred_writes_lat $bluestore_kv_queue_time $bluestore_kv_sync_lat $bluestore_kvq_lat $bluestore_simple_service_lat $bluestore_deferred_service_lat | paste -sd ',' >> ${DATA_FILE} 
-    done
-    #sudo bin/ceph daemon osd.0 perf reset osd
 }
 
-#for j in 1 2 3 4 5; do
 printf '%s\n' "bs" "runtime" "qdepth" "bw_mbs" "lat_s" "osd_lat" "op_queue_lat" "bluestore_simple_writes_lat" "bluestore_deferred_writes_lat" "kv_queue_lat" "bluestore_kv_sync_lat" "bluestore_kvq_lat" "bluestore_simple_service_lat" "bluestore_deferred_service_lat" |  paste -sd ',' > ${DATA_FILE} 
-#"kv_sync_p99_lat" "kv_sync_p95_lat" "kv_sync_median_lat" "kv_sync_min_lat" "kvq_p99_lat" "kvq_p95_lat" "kvq_median_lat" "kvq_min_lat" |  paste -sd ',' > ${DATA_FILE}
 #for qd in {16..72..8}; do
 for qd in 48; do
 	#bs="$((2**i*4*1024))"
 	#iototal="$((2**i*4*1024*100000))"   #"$((2**i*40))m"
+	
 	./start_ceph.sh
 	sudo bin/ceph osd pool create mybench 128 128
 	sudo bin/rbd create --size=40G mybench/image1
-	sleep 10 # warmup
+
+	sleep 5 # warmup
+
+	# change the fio parameters
 	sed -i "s/iodepth=.*/iodepth=${qd}/g" fio_write.fio
 	sed -i "s/bs=.*/bs=${bs}/g" fio_write.fio
 	sed -i "s/rw=.*/rw=${rw}/g" fio_write.fio
@@ -59,8 +53,10 @@ for qd in 48; do
         
     echo benchmark starts!
 	echo $qd
-    sudo LD_LIBRARY_PATH="$CEPH_HOME"/build/lib:$LD_LIBRARY_PATH "$FIO_HOME"/fio fio_write.fio --output-format=json | tee fio-bench-${qd}.json #--output=dump-fio-bench-${qd}.json 
+    sudo LD_LIBRARY_PATH="$CEPH_HOME"/build/lib:$LD_LIBRARY_PATH "$FIO_HOME"/fio fio_write.fio --output-format=json --output=dump-fio-bench-${qd}.json 
+	
 	sleep 5
+
 	sudo bin/ceph daemon osd.0 dump kvq vector
 	mv ./kvq_lat_vec.csv ./dump_kvq_lat_vec-${qd}.csv
 	mv ./kv_sync_lat_vec.csv ./dump_kv_sync_lat_vec-${qd}.csv
@@ -70,36 +66,19 @@ for qd in 48; do
 	mv ./bluestore_deferred_writes_lat_vec.csv ./dump_bluestore_deferred_writes_lat_vec-${qd}.csv
 	mv ./bluestore_simple_service_lat_vec.csv ./dump_bluestore_simple_service_lat_vec-${qd}.csv
 	mv ./bluestore_deferred_service_lat_vec.csv ./dump_bluestore_deferred_service_lat_vec-${qd}.csv
-	mv ./kv_queue_size_vec.csv ./dump_kv_queue_size_vec-${qd}.csv
-	#mv ./blocking_dur_vec.csv ./dump_blocking_dur_vec-${qd}.csv
-        # it contains kvq_p99_lat, kvq_p95_lat, kvq_median_lat, kvq_min_lat, kv_sync_p99_lat, kv_sync_p95_lat, kv_sync_median_lat, kv_sync_min_lat
-        # process kvq_lat.csv and kv_cync_lat.csv
-	#INPUTCSV=dump_kvq_lat_analysis_vec-${qd}.csv
-        #kvq_p99_lat=$(awk 'BEGIN { FS = "," } ; NR == 2{ print $1 }' < $INPUTCSV)	
-
+	mv ./kv_queue_size_vec.csv ./dump_kv_queue_size_vec-${qd}.csv	
 
 	sudo bin/ceph daemon osd.0 dump opq vector
 	mv ./opq_vec.csv ./dump_opq_size_vec-${qd}.csv
-	#mv ./opq_lat_vec.csv ./dump_opq_lat_vec-${qd}.csv
-
 
 	single_dump $qd
-        echo benchmark stops!
+    echo benchmark stops!
 
-        #export PLOTOUTNAME="dump-block-dur-${qd}.png"
-        #export PLOTINNAME="dump-block-dur-${qd}.csv"
-        #python plot_codel.py 
 	sudo bin/rbd rm mybench/image1
-        sudo bin/ceph osd pool delete $pool $pool --yes-i-really-really-mean-it
-        sudo ../src/stop.sh
-	#sudo bin/init-ceph --verbose stop
-	#sudo bin/init-ceph stop
-	#sleep 5
-	#sudo rm -rf dev out
-	#sudo rm -rf ceph.conf
+    sudo bin/ceph osd pool delete $pool $pool --yes-i-really-really-mean-it
+    sudo ../src/stop.sh
+	
 done
-
-#python plot-codel.py
 
 # move everything to a directory
 dn=queueing-delay-${rw}-$(date +"%Y_%m_%d_%I_%M_%p")
